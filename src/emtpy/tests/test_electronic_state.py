9d595af587d5c58e36ef090fef2a714f64dd515a
duncan <duncan.parris@gmail.com>
2013-04-25 22:35:32 +0100

    Test outline for electronic state

---
+++ src/emtpy/tests/test_electronic_state.py
@@ -0,0 +1,31 @@
+__author__ = 'duncan'
+from nose.tools import istest
+
+
+class TestElectronicState(object):
+    """
+        Tests for the electronic state class. This could do with a set up method for creating a valid state
+    """
+
+    @istest
+    def test_expected_pos_for_nice_guassian(self):
+        pass
+
+
+    @istest
+    def test_expected_pos_for_nice_guassian_overlapping_edge(self):
+        pass
+
+
+    @istest
+    def test_expected_pos_for_nice_guassian_in_corner(self):
+        pass
+
+    @istest
+    def test_expected_pos_for_flat_function(self):
+        pass
+
+
+    @istest
+    def test_expected_pos_for_spiky_function(self):
+        pass
