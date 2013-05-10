db598330b9d9cc1a4a2c043fd40dc044208f84ed
Duncan Watson-Parris <duncan.watson-parris@tessella.com>
2013-05-10 13:11:37 +0100

    Added a symmetric new yale sparse matrix storage scheme and tests for both versions, all of which pass

--- src/emtpy/tests/test_yale_sparse.py
+++ src/emtpy/tests/test_yale_sparse.py
@@ -1,20 +1,65 @@
-from numpy.core.numeric import array_equal
-
 __author__ = 'duncan'
 from nose.tools import istest, eq_
+from numpy.testing.utils import assert_array_equal
+import numpy as np
+from emtpy.yale_sparse import YaleSparse, YaleSparseSymmetric
 
 
-@istest
-def test_constructing_ysm():
-    import numpy as np
-    from emtpy.yale_sparse import YaleSparse
-    dense = np.array([[3.0,0.0,1.0,0.0,0.0],
-                      [0.0,4.0,0.0,0.0,0.0],
-                      [0.0,7.0,5.0,9.0,0.0],
-                      [0.0,0.0,0.0,0.0,2.0],
-                      [0.0,0.0,0.0,6.0,5.0]])
-    sp = YaleSparse.from_dense(dense,15)
-    ref_ija = np.array([7,8,8,10,11,12,3,2,4,5,4])
-    ref_sa = np.array([3.0,4.0,5.0,0.0,5.0,0.0,1.0,7.0,9.0,2.0,6.0])
-    assert array_equal(ref_ija, sp.ija)
-    assert array_equal(ref_sa, sp.sa)
+class TestYaleSparse(object):
+
+    def __init__(self):
+        self.dense = np.array([[3.0, 0.0, 1.0, 0.0, 0.0],
+                               [0.0, 4.0, 0.0, 0.0, 0.0],
+                               [0.0, 7.0, 5.0, 9.0, 0.0],
+                               [0.0, 0.0, 0.0, 0.0, 2.0],
+                               [0.0, 0.0, 0.0, 6.0, 5.0]])
+    @istest
+    def test_constructing_ysm(self):
+        sp = YaleSparse.from_dense(self.dense, 11)
+        ref_ija = np.array([6, 7, 7, 9, 10, 11, 2, 1, 3, 4, 3])
+        ref_sa = np.array([3.0, 4.0, 5.0, 0.0, 5.0, 0.0, 1.0, 7.0, 9.0, 2.0, 6.0])
+        assert_array_equal(ref_ija, sp.ija)
+        assert_array_equal(ref_sa, sp.sa)
+
+    @istest
+    def test_deconstructing_ysm(self):
+        sp = YaleSparse.from_dense(self.dense, 11)
+        new_dense = sp.to_dense((5, 5))
+        assert_array_equal(new_dense, self.dense)
+
+
+    @istest
+    def test_ysm_right_multiplication(self):
+        test_vector = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+        sp = YaleSparse.from_dense(self.dense, 11)
+        assert_array_equal(sp.av(test_vector), np.dot(self.dense, test_vector))
+
+class TestYaleSparseSymmetric(object):
+
+    def __init__(self):
+        self.dense = np.array([[3.0, 0.0, 1.0, 0.0, 0.0],
+                               [0.0, 4.0, 7.0, 0.0, 0.0],
+                               [1.0, 7.0, 5.0, 9.0, 0.0],
+                               [0.0, 0.0, 9.0, 0.0, 6.0],
+                               [0.0, 0.0, 0.0, 6.0, 5.0]])
+    @istest
+    def test_constructing_ysm(self):
+        sp = YaleSparseSymmetric.from_dense(self.dense, 10)
+        ref_ija = np.array([6, 7, 8, 9, 10, 10, 2, 2, 3, 4])
+        ref_sa = np.array([3.0, 4.0, 5.0, 0.0, 5.0, 0.0, 1.0, 7.0, 9.0, 6.0])
+        assert_array_equal(ref_ija, sp.ija)
+        assert_array_equal(ref_sa, sp.sa)
+
+    @istest
+    def test_deconstructing_ysm(self):
+        sp = YaleSparseSymmetric.from_dense(self.dense, 10)
+        new_dense = sp.to_dense((5, 5))
+        assert_array_equal(new_dense, self.dense)
+
+
+    @istest
+    def test_ysm_right_multiplication(self):
+        test_vector = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
+        sp = YaleSparseSymmetric.from_dense(self.dense, 10)
+        assert_array_equal(sp.av(test_vector), np.dot(self.dense, test_vector))
+
--- src/emtpy/yale_sparse.py
+++ src/emtpy/yale_sparse.py
@@ -10,7 +10,7 @@ class YaleSparse(object):
     def from_dense(cls, dense_matrix, nmax, thresh=0.0):
         """
         Converts a square matrix a(1:n,1:n) with physical dimension np into row-indexed sparse
-        storage mode. Only elements of a with magnitude ≥thresh are retained. Output is in
+        storage mode. Only elements of a with magnitude greater than thresh are retained. Output is in
         two linear arrays with physical dimension nmax (an input parameter): sa(1:) contains
         array values, indexed by ija(1:). The logical sizes of sa and ija on output are both
         ija(ija(1)-1)-1 (see text).
@@ -20,27 +20,43 @@ class YaleSparse(object):
         ija = np.zeros(nmax, dtype=np.int8)
 
         n = dense_matrix.shape[0]
-        # Store diagonal elements.
 
+        # Store diagonal elements.
         sa[:n] = dense_matrix.diagonal()
-        ija[1]=n+2
+        ija[0] = n + 1
+
         # Index to 1st row off-diagonal element, if any.
-        k=n+1
+        k = n
+
         # Loop over rows.
-        for i in range(1,n):
-            for j in range(1,n):
+        for i in range(n):
+            for j in range(n):
                 # Loop over columns.
-                if(abs(dense_matrix[i,j]).ge.thresh):
+                if(abs(dense_matrix[i, j]) > thresh):
                     if(i != j):
                         # Store off-diagonal elements and their columns.
-                        k=k+1
+                        k = k + 1
                         if(k > nmax): raise ValueError("nmax too small in sprsin")
-                        sa[k]=dense_matrix[i,j]
-                        ija[k]=j
-            ija[i+1]=k+1
+                        sa[k] = dense_matrix[i, j]
+                        ija[k] = j
+            ija[i+1] = k + 1
             # As each row is completed, store index to next.
 
         return cls(ija, sa)
+
+    def to_dense(self, shape):
+        import numpy as np
+        dense = np.zeros(shape)
+        #diag = np.diag_indices_from(dense)
+
+        n = shape[0]
+
+        for i in range(n):
+            dense[i, i] = self.sa[i]
+            for k in range(self.ija[i], (self.ija[i+1])):
+                dense[i, self.ija[k]] = self.sa[k]
+
+        return dense
 
     def av(self, v):
         import numpy as np
@@ -48,12 +64,89 @@ class YaleSparse(object):
         y = np.zeros(n, dtype=np.float64)
 
         for i in range(n):
-           y[i] = self.sa[i]*v[i]
-           for j in range (self.ija[i], (self.ija[i+1]-2)):
-              y[i] += self.sa[j]*v[self.ija[j]]
+            y[i] = self.sa[i] * v[i]
+            for k in range(self.ija[i], (self.ija[i+1])):
+                y[i] += self.sa[k] * v[self.ija[k]]
+
+        # for i in range(n):
+        #     for j in range (self.ija[i], (self.ija[i+1]-2)):
+        #         y[self.ija[j]] = y[self.ija[j]] + self.sa[j]*v[i] #Use for symetric storage
+
+        return y
+
+
+class YaleSparseSymmetric(object):
+
+    def __init__(self, ija, sa):
+        self.ija = ija
+        self.sa = sa
+
+    @classmethod
+    def from_dense(cls, dense_matrix, nmax, thresh=0.0):
+        """
+        Converts a square matrix a(1:n,1:n) with physical dimension np into row-indexed sparse
+        storage mode. Only elements of a with magnitude greater than thresh are retained. Output is in
+        two linear arrays with physical dimension nmax (an input parameter): sa(1:) contains
+        array values, indexed by ija(1:). The logical sizes of sa and ija on output are both
+        ija(ija(1)-1)-1 (see text).
+        """
+        import numpy as np
+        sa = np.zeros(nmax, dtype=np.float64)
+        ija = np.zeros(nmax, dtype=np.int8)
+
+        n = dense_matrix.shape[0]
+
+        # Store diagonal elements.
+        sa[:n] = dense_matrix.diagonal()
+        ija[0] = n + 1
+
+        # Index to 1st row off-diagonal element, if any.
+        k = n
+
+        # Loop over rows.
+        for i in range(n):
+            for j in range(i+1,n):
+                # Loop over columns.
+                if(abs(dense_matrix[i, j]) > thresh):
+                    # Store off-diagonal elements and their columns.
+                    k = k + 1
+                    if(k > nmax): raise ValueError("nmax too small in sprsin")
+                    sa[k] = dense_matrix[i, j]
+                    ija[k] = j
+            ija[i+1] = k + 1
+            # As each row is completed, store index to next.
+
+        return cls(ija, sa)
+
+    def to_dense(self, shape):
+        import numpy as np
+        dense = np.zeros(shape)
+        #diag = np.diag_indices_from(dense)
+
+        n = shape[0]
 
         for i in range(n):
-           for j in range (self.ija[i], (self.ija[i+1]-2)):
-              y[self.ija[j]] = y[self.ija[j]] + self.sa[j]*v[i] #Use for symetric storage
+            dense[i, i] = self.sa[i]
+            for k in range(self.ija[i], (self.ija[i+1])):
+                j = self.ija[k]
+                dense[i, j] = self.sa[k]
+                dense[j, i] = dense[i, j]
+
+        return dense
+
+    def av(self, v):
+        import numpy as np
+        n = len(v)
+        y = np.zeros(n, dtype=np.float64)
+
+        for i in range(n):
+            y[i] = self.sa[i] * v[i]
+            for k in range(self.ija[i], (self.ija[i+1])):
+                y[i] += self.sa[k] * v[self.ija[k]]
+
+
+        for i in range(n):
+            for k in range(self.ija[i], (self.ija[i+1])):
+                y[self.ija[k]] += self.sa[k]*v[i] #Use for symetric storage
 
         return y
